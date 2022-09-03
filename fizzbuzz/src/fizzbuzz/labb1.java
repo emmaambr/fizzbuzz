35917b1c971017cfd4fe6480d6df8ecf982c9258
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-09-03 14:38:03 +0200

    labb 1 complete

---
+++ fizzbuzz/src/fizzbuzz/labb1.java
@@ -0,0 +1,21 @@
+package fizzbuzz;
+
+public class labb1 {
+    public static void main(String[] args) {
+        int i;
+
+        for(i = 1; i <= 100; i++) {
+            if (i == 42) {
+                System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
